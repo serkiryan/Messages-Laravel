1fcacc6b998e5ff3ce0c43862654ff4ba309c3ea
Seryog Kirakosyan <seryogkirakosyan@gmail.com>
2017-03-26 14:32:02 +0400

    Laravel 5.0 install

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,52 @@
+<html>
+	<head>
+		<title>Laravel</title>
+		
+		<link href='//fonts.googleapis.com/css?family=Lato:100' rel='stylesheet' type='text/css'>
+
+		<style>
+			body {
+				margin: 0;
+				padding: 0;
+				width: 100%;
+				height: 100%;
+				color: #B0BEC5;
+				display: table;
+				font-weight: 100;
+				font-family: 'Arial';
+			}
+
+			.container {
+				text-align: center;
+				display: table-cell;
+				vertical-align: middle;
+			}
+
+			.content {
+				text-align: center;
+				display: inline-block;
+			}
+
+			.title {
+				font-size: 96px;
+				margin-bottom: 40px;
+			}
+
+			.quote {
+				font-size: 36px;
+			}
+                        .quote a{
+                                /*text-decoration: none;*/
+                                font-weight: bold;
+                        }
+		</style>
+	</head>
+	<body>
+		<div class="container">
+			<div class="content">
+				<div class="title">Laravel 5</div>
+                                <div class="quote"><a href="#" class="btn btn-success">HTML version of Messages</a></div>
+			</div>
+		</div>
+	</body>
+</html>
